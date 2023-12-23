f2ff2c1a361180c5ec1235378efd54fa81480f92
Theophile Thiberge <tthiberge@gmail.com>
2023-12-23 09:33:40 +0100

    Add a few routes to the app.js

--- mvg-backend/app.js
+++ mvg-backend/app.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const express = require('express')
+const Book = require('./models/book')
+const User = require('./models/user')
 
 const app = express()
 app.use(express.json()); // Récup formulaire dans req.body
@@ -22,5 +24,29 @@ app.use((req, res, next) => {
   ); // certaines méthodes
   next(); // Après vérification, passage aux middlewares suivants
 });
+
+app.get('/api/books', (req, res) => {
+  Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json('Error:' + error))
+});
+
+app.get('/api/books/:id', (req, res) => {
+  Book.findOne({_id: req.params.id})
+  .then( book => res.status(200).json(book) )
+  .catch( error => res.status(400).json('Error:' + error))
+})
+
+app.get('/api/books/bestrating', (req, res) => {
+  Book.find()
+  .sort({ averageRating: -1 }) // Sort in descending order based on averageRating
+  .limit(3) // Limit the result to the top 3 books
+  .then(bestBooks => res.status(200).json(bestBooks))
+  .catch(error => res.status(400).json('Error:' + error));
+})
+
+
+
+
 
 module.exports = app
