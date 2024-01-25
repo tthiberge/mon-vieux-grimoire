ca600a5a070d71ff25c292d45215a5f703502bfe
Theophile Thiberge <tthiberge@gmail.com>
2024-01-25 22:24:40 +0100

    Mise à jour des variables d'environnement pour une connexion en local

--- mvg-backend/app.js
+++ mvg-backend/app.js
@@ -7,15 +7,15 @@ const booksRoutes = require('./routes/books')
 
 const app = express()
 
-const username = process.env.ATLAS_USERNAME
-const password = process.env.ATLAS_PASSWORD
-const clusterReference = process.env.ATLAS_CLUSTER_REFERENCE
+const username = process.env.DB_USERNAME
+const password = process.env.DB_PASSWORD
+const host = process.env.DB_HOST
+const dbName = process.env.DB_NAME
 
 app.use(express.json()); // Récup formulaire dans req.body
 mongoose
   .connect(
-    `mongodb+srv://${username}:${password}@${clusterReference}.mongodb.net/?retryWrites=true&w=majority`) // finalement réussi à me connecter...
-    // "mongodb+srv://theo:NodeQuiz@node-quiz.llttoxg.mongodb.net/?retryWrites=true&w=majority")
+    `mongodb+srv://${username}:${password}@${host}/${dbName}?retryWrites=true&w=majority`)
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch((error) => console.log("Connexion à MongoDB échouée !", error));
 
